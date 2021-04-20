03ef813061622fd60f1fba6a7ebba5b6b9bd4e72
suida <suidar@foxmail.com>
2021-04-20 16:32:06 +0800

    Add full material-ui support and costumized navbar

--- rdx/src/App.tsx
+++ rdx/src/App.tsx
@@ -1,56 +1,37 @@
-import React from 'react';
-import logo from '@/logo.svg';
-import { Counter } from '@/features/counter/Counter';
-import './App.css';
+import {
+  Link,
+  Switch,
+  Route,
+} from 'react-router-dom';
+import Icon from '@material-ui/core/Icon'
+import './App.scss';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <Counter />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <span>
-          <span>Learn </span>
-          <a
-            className="App-link"
-            href="https://reactjs.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            React
-          </a>
-          <span>, </span>
-          <a
-            className="App-link"
-            href="https://redux.js.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Redux
-          </a>
-          <span>, </span>
-          <a
-            className="App-link"
-            href="https://redux-toolkit.js.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Redux Toolkit
-          </a>
-          ,<span> and </span>
-          <a
-            className="App-link"
-            href="https://react-redux.js.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            React Redux
-          </a>
-        </span>
+        <nav className="navbar">
+          <ul className="nav-list">
+            <li>
+              <Link to="/">主页</Link>
+            </li>
+            <li>
+              <Link to="/article">文章</Link>
+            </li>
+            <li>
+              <Link to="/lab">实验场</Link>
+            </li>
+            <li>
+              <Link to="/category">其他 </Link>
+            </li>
+          </ul>
+        </nav>
+        <input className="search" type="search" />
       </header>
+      <main>
+
+      </main>
+      <footer></footer>
     </div>
   );
 }
