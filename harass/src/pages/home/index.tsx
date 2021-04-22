570e7444466932a20da42c30b047c8eefc8883fc
suida <suidar@foxmail.com>
2021-04-22 16:20:45 +0800

    :sparkles: (pages/home): Initialize home page with grid layout

--- harass/src/pages/home/index.tsx
+++ harass/src/pages/home/index.tsx
@@ -1,9 +1,40 @@
-import { Button } from '@material-ui/core';
+import {
+  Button,
+  Grid,
+  makeStyles,
+  createStyles,
+  Theme,
+} from '@material-ui/core';
+
+const spacingFactor = 2;
+
+const useStyles = makeStyles((theme: Theme) => createStyles({
+  root: {
+    padding: `${theme.spacing(spacingFactor) / 2}px`,
+  },
+  main: {
+    backgroundColor: "#888888",
+    minHeight: "2000px"
+  },
+  aside: {
+    backgroundColor: "#288888",
+    minHeight: "2000px"
+  },
+}));
 
 export default () => {
+  const classes = useStyles();
+
   return (
-    <div>
-      <Button variant="contained" color="primary">Hello, world</Button>
+    <div className={classes.root}>
+      <Grid container spacing={spacingFactor} alignItems={"center"} justify={"center"}>
+        <Grid className={classes.main} item xs={12} md={9} lg={6}>
+
+        </Grid>
+        <Grid className={classes.aside} item xs={12} md={3}>
+
+        </Grid>
+      </Grid>
     </div>
   )
 }
---
+++ harass/src/pages/lab/index.tsx
@@ -0,0 +1,9 @@
+import { Button } from '@material-ui/core';
+
+export default () => {
+  return (
+    <div>
+      <Button variant="contained" color="primary">Hello, world</Button>
+    </div>
+  )
+}
