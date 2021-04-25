600724f5ed132cd42d71e1856c2711d305d76576
suida <suidar@foxmail.com>
2021-04-25 19:49:32 +0800

    :sparkles: (harass/components): Implement component article card

--- harass/src/components/Article.tsx
+++ harass/src/components/Article.tsx
@@ -1,23 +1,89 @@
-import { HTMLProps, FC } from 'react';
+import { FC } from 'react';
 import dayjs from 'dayjs';
-import { Card } from '@material-ui/core';
+import cls from 'classnames';
+import {
+  Card,
+  CardHeader,
+  CardContent,
+  CardActions,
+  CardProps,
+  Typography,
+  IconButton,
+  Icon,
+  makeStyles,
+  createStyles,
+  Theme,
+} from '@material-ui/core';
+import { formatDateTime } from '@/utils';
+
+const useStyles = makeStyles((theme: Theme) => 
+  createStyles({
+    root: {
+      width: '100%',
+      marginBottom: '10px',
+    },
+    content: {
+      paddingTop: '0px',
+      paddingBottom: '0px',
+    },
+    actions: {
+      paddingTop: '0px',
+      '& .MuiIcon-root': {
+        fontSize: '1.2rem',
+      },
+    },
+    detailButton: {
+      marginLeft: 'auto',
+      '& .MuiIcon-root': {
+        fontSize: '1.5rem',
+      },
+    },
+  }),
+);
 
 export interface Article {
   id: number,
   title: string,
   rawContent: string,
   author: number,
-  createAt: dayjs.Dayjs,
+  createdAt: dayjs.Dayjs,
   modifiedAt: dayjs.Dayjs,
 };
 
-export interface ArticleCardProps extends HTMLProps<any> {
+export interface ArticleCardProps extends CardProps {
   article: Article;
 }
 
-export const ArticleCard: FC<ArticleCardProps> = (props) => {
+export const ArticleCard: FC<ArticleCardProps> = ({
+  article,
+  className,
+  ...rest
+}) => {
+  const classes = useStyles();
+
   return (
-    <Card>
+    <Card variant="outlined" className={cls(classes.root, className)} {...rest}>
+      <CardHeader
+        title={<Typography variant="h5">{article.title}</Typography>}
+        subheader={<Typography variant="body2">落笔于 {formatDateTime(article.createdAt)}</Typography>}
+      />
+      <CardContent className={classes.content}>
+        <Typography variant="body1" component="p">
+          {article.rawContent.slice(0, 150)}
+          <b> . . .</b>
+        </Typography>
+      </CardContent>
+      <CardActions disableSpacing className={classes.actions}>
+        <IconButton>
+          <Icon>thumb_up</Icon>
+        </IconButton>
+        <IconButton>
+          <Icon>share</Icon>
+        </IconButton>
+        <IconButton className={classes.detailButton}>
+          <Icon>east</Icon>
+        </IconButton>
+      </CardActions>
     </Card>
   )
 }
--- harass/src/pages/home/index.tsx
+++ harass/src/pages/home/index.tsx
@@ -1,13 +1,16 @@
 import { useEffect, } from 'react';
 import {
   Grid,
+  Paper,
+  Button,
+  Divider,
   makeStyles,
   createStyles,
   Theme,
 } from '@material-ui/core';
 import { useAppDispatch, useAppSelector } from '@/store/hooks';
 import { ArticleCard, Article } from '@/components/Article';
-import { selectArticles, } from './slice';
+import { selectArticles, updateArticles } from './slice';
 import { fetchArticles, } from './saga';
 
 const spacingFactor = 2;
@@ -17,12 +20,10 @@ const useStyles = makeStyles((theme: Theme) => createStyles({
     padding: `${theme.spacing(spacingFactor) / 2}px`,
   },
   main: {
-    backgroundColor: "#888888",
-    minHeight: "2000px"
+    maxWidth: "1200px",
   },
   aside: {
-    backgroundColor: "#288888",
-    minHeight: "2000px"
+    maxWidth: "600px",
   },
 }));
 
@@ -32,21 +33,32 @@ export default () => {
   const articles = useAppSelector(selectArticles);
 
   useEffect(() => {
-    dispatch(fetchArticles());
-  }, []);
-
-  console.log(articles);
+      if (articles.length === 0) dispatch(fetchArticles());
+  });
 
   return (
     <div className={classes.root}>
-      <Grid container spacing={spacingFactor} alignItems={"center"} justify={"center"}>
-        <Grid className={classes.main} item xs={12} md={9} lg={6}>
-          {articles && articles.map((article: Article) => {
-            <ArticleCard article={article} />
-          })}
+      <Grid container spacing={spacingFactor} justify={"center"}>
+        <Grid className={classes.main} item xs={12} sm={9} lg={6}>
+          {articles && articles.map((article: Article, idx) => (
+            <ArticleCard
+              article={article}
+              key={idx}
+            />
+          ))}
         </Grid>
-        <Grid className={classes.aside} item xs={12} md={3}>
-
+        <Grid className={classes.aside} item xs={12} sm={3}>
+            <Button
+              variant="contained"
+              color="primary"
+              onClick={() => dispatch(updateArticles([...articles, ...articles]))}
+            >Clone Articles</Button>
+            <Divider />
+            <Button
+              variant="contained"
+              color="primary"
+              onClick={() => dispatch(fetchArticles())}
+            >Fetch Articles</Button>
         </Grid>
       </Grid>
     </div>
